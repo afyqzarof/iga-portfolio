25d23b96d8910b3a0ee56e73f44dcb30312094af
afyqzarof <norafyqe@gmail.com>
2026-04-20 13:48:51 +0800

    add menu component

---
+++ app/components/menu.tsx
@@ -0,0 +1,7 @@
+const Menu = () => {
+  return (
+    <article className="fixed bottom-4 text-white font-serif">menu</article>
+  );
+};
+
+export { Menu };
--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,3 +1,4 @@
+import { Menu } from "~/components/menu";
 import type { Route } from "./+types/home";
 
 export function meta({}: Route.MetaArgs) {
@@ -10,10 +11,11 @@ export function meta({}: Route.MetaArgs) {
 const Home = () => {
   return (
     <main className="h-screen bg-cover bg-[url(/backgrounds/mobile-home-bg.jpg)] p-4">
-      <section className="mt-28">
+      <section className="mt-24">
         <h1 className="font-mono text-white text-3xl">iga sokół,</h1>
         <h2 className="font-mono text-white text-3xl">UI/UX designer</h2>
       </section>
+      <Menu />
     </main>
   );
 };
