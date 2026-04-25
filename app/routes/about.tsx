1d7164194d9cc931ef2c2805386361749cf421a3
afyqzarof <norafyqe@gmail.com>
2026-04-25 18:58:06 +0800

    add first pargraph to about page

--- app/routes/about.tsx
+++ app/routes/about.tsx
@@ -11,7 +11,13 @@ export function meta({}: Route.MetaArgs) {
 const About = () => {
   return (
     <main className="fixed top-0 h-screen w-screen bg-[url(/backgrounds/mobile-about-bg.jpg)] bg-cover">
-      <p className="text-white">about page</p>
+      <div className="py-24 pr-20 pl-4 font-serif">
+        <p className="text-white">
+          Iga holds a degree in graphic communication design. Her route into
+          UI/UX took her through  project management, operations, and  internal
+          communications.
+        </p>
+      </div>
       <Menu />
     </main>
   );
