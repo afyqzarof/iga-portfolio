bb8afc9cb03030be02b410b6de9d8edd47ad7033
afyqzarof <norafyqe@gmail.com>
2026-04-25 18:30:29 +0800

    udpate bg for mobil about page

--- app/routes/about.tsx
+++ app/routes/about.tsx
@@ -1,9 +1,17 @@
 import { Menu } from "~/components/menu";
+import type { Route } from "./+types/about";
+
+export function meta({}: Route.MetaArgs) {
+  return [
+    { title: "iga's about page" },
+    { name: "description", content: "about page" },
+  ];
+}
 
 const About = () => {
   return (
-    <main>
-      <p>This is about page</p>
+    <main className="fixed top-0 h-screen w-screen bg-[url(/backgrounds/mobile-about-bg.jpg)] bg-cover">
+      <p className="text-white">about page</p>
       <Menu />
     </main>
   );
