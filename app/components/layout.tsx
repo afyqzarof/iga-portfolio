242418ec065b95209fb7777cb28c8fec5dd34e5f
afyqzarof <norafyqe@gmail.com>
2026-04-25 23:37:18 +0800

    update layout.tsx for desktop

--- app/components/layout.tsx
+++ app/components/layout.tsx
@@ -1,5 +1,6 @@
-import { Outlet, useLocation } from "react-router";
+import { NavLink, Outlet, useLocation } from "react-router";
 import { Menu } from "./menu";
+import { Dot } from "./dot";
 
 const Layout = () => {
   const location = useLocation();
@@ -13,13 +14,53 @@ const Layout = () => {
     }
   };
 
+  const paths = ["home", "work", "about", "contact"];
+
   return (
-    <main
-      className={`fixed top-0 h-screen w-screen bg-[url(/backgrounds/mobile/${getPath()}.jpg)] bg-cover md:bg-[url(/backgrounds/desktop/home.jpg)]`}
-    >
-      <Outlet />
-      <Menu />
-    </main>
+    <>
+      {/* mobile layout */}
+      <main
+        className={`fixed top-0 h-screen w-screen bg-[url(/backgrounds/mobile/${getPath()}.jpg)] bg-cover md:hidden`}
+      >
+        <Outlet />
+        <Menu />
+      </main>
+
+      {/* desktop layout */}
+      <main
+        className={`fixed top-0 hidden h-screen w-screen bg-cover px-8 py-4 md:flex md:bg-[url(/backgrounds/desktop/home.jpg)]`}
+      >
+        <section className="flex flex-col justify-between font-mono">
+          <div className="flex-1/3">
+            <p className="mt-34 flex items-center gap-4 text-3xl text-white">
+              <Dot className="bg-white" />
+              {getPath()}
+            </p>
+          </div>
+          <div className="text-accent flex-1">
+            <h1 className="font-mono text-4xl">iga sokół</h1>
+            <h2 className="font-mono text-4xl">UI/UX designer</h2>
+          </div>
+          <ul className="flex flex-1 flex-col gap-4 pb-8 pl-8">
+            {paths
+              .filter((path) => path !== getPath())
+              .map((path) => {
+                return (
+                  <li className="text-3xl text-white">
+                    <NavLink
+                      to={path === "home" ? "/" : path}
+                      className="flex items-center gap-2"
+                    >
+                      {path}
+                    </NavLink>
+                  </li>
+                );
+              })}
+          </ul>
+        </section>
+        <Outlet />
+      </main>
+    </>
   );
 };
 
--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,4 +1,3 @@
-import { Menu } from "~/components/menu";
 import type { Route } from "./+types/home";
 
 export function meta({}: Route.MetaArgs) {
@@ -10,9 +9,9 @@ export function meta({}: Route.MetaArgs) {
 
 const Home = () => {
   return (
-    <div className="p-4">
+    <div className="p-4 md:hidden">
       <section className="mt-24">
-        <h1 className="font-mono text-3xl text-white">iga sokół,</h1>
+        <h1 className="font-mono text-3xl text-white">iga sokół</h1>
         <h2 className="font-mono text-3xl text-white">UI/UX designer</h2>
       </section>
     </div>
