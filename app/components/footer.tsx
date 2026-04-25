1a569d231731b8b31d0f727b8819a4d95fed04ee
afyqzarof <norafyqe@gmail.com>
2026-04-25 19:08:10 +0800

    style about/menu

---
+++ app/components/footer.tsx
@@ -0,0 +1,11 @@
+const Footer = () => {
+  return (
+    <footer>
+      <p className="text-tertiary font-mono text-xs">
+        © iga sokół, 2026 | this website was brought to life kindly by afyq.xyz
+      </p>
+    </footer>
+  );
+};
+
+export default Footer;
--- app/components/menu.tsx
+++ app/components/menu.tsx
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import { Link, NavLink, useLocation } from "react-router";
 import { Dot } from "./dot";
+import Footer from "./footer";
 
 const MenuBtn = ({
   handleClick,
@@ -36,30 +37,33 @@ const Menu = () => {
         </article>
       )}
       <nav
-        className={` ${open ? "translate-0 opacity-80" : "translate-y-full opacity-0"} absolute top-0 flex h-screen w-screen items-end overflow-hidden bg-[#6B6B6BCC] p-4 font-mono text-xl text-white transition`}
+        className={` ${open ? "translate-0 opacity-80 backdrop-blur-2xl" : "translate-y-full opacity-0"} absolute top-0 flex h-screen w-screen items-end overflow-hidden bg-[#6B6B6BCC] p-4 font-mono text-xl text-white transition`}
       >
-        <section className="flex flex-col items-start gap-5">
-          <ul className="flex flex-col gap-5">
-            <li>
-              <NavLink to="/" className={isActive("/") ? "line-through" : ""}>
-                home
-              </NavLink>
-            </li>
-            <li>view my project, studio</li>
-            <li>
-              <Link
-                to="about"
-                className={isActive("/about") ? "line-through" : ""}
-              >
-                about
-              </Link>
-            </li>
-            <li>contact</li>
-          </ul>
-          <div className="text-accent">
-            <MenuBtn handleClick={handleClick} isActive={open} />
-          </div>
-        </section>
+        <div className="flex h-full flex-col justify-between">
+          <Footer />
+          <section className="flex flex-col items-start gap-5">
+            <ul className="flex flex-col gap-5">
+              <li>
+                <NavLink to="/" className={isActive("/") ? "line-through" : ""}>
+                  home
+                </NavLink>
+              </li>
+              <li>view my project, studio</li>
+              <li>
+                <Link
+                  to="about"
+                  className={isActive("/about") ? "line-through" : ""}
+                >
+                  about
+                </Link>
+              </li>
+              <li>contact</li>
+            </ul>
+            <div className="text-accent">
+              <MenuBtn handleClick={handleClick} isActive={open} />
+            </div>
+          </section>
+        </div>
       </nav>
     </>
   );
