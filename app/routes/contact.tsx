37a19a0a8beb5d6b61bf468b3fa33910aa213b34
afyqzarof <norafyqe@gmail.com>
2026-05-03 15:15:55 +0800

    add contact page

--- app/routes/contact.tsx
+++ app/routes/contact.tsx
@@ -8,15 +8,16 @@ export function meta({}: Route.MetaArgs) {
 }
 const Contact = () => {
   return (
-    <div className="p-8 md:hidden">
-      <section className="mt-36">
-        <h1 className="font-mono text-xl text-white">email me at:</h1>
+    <div className="p-8 md:flex md:h-[63%] md:w-full md:items-end md:justify-end">
+      <section className="mt-36 font-serif">
+        <p className="text-2xl text-white md:hidden">email me at:</p>
         <a
           href="mailto:igaa.sokol@gmail.com"
-          className="font-mono text-xl text-white underline underline-offset-4 hover:no-underline"
+          className="text-2xl text-white underline underline-offset-4 hover:no-underline"
         >
           igaa.sokol@gmail.com
         </a>
+        <p className="hidden text-2xl text-white md:flex">london, uk</p>
       </section>
     </div>
   );
