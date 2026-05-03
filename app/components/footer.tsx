171e68e21c8df115c32de110d638005a996c413f
afyqzarof <norafyqe@gmail.com>
2026-05-03 14:51:52 +0800

    add afyq github link

--- app/components/footer.tsx
+++ app/components/footer.tsx
@@ -2,7 +2,13 @@ const Footer = () => {
   return (
     <footer>
       <p className="text-tertiary font-mono text-xs">
-        © iga sokół, 2026 | this website was brought to life kindly by afyq.xyz
+        © iga sokół, 2026 | this website was brought to life kindly by{" "}
+        <a
+          href="https://github.com/afyqzarof"
+          className="underline hover:no-underline"
+        >
+          afyq
+        </a>
       </p>
     </footer>
   );
